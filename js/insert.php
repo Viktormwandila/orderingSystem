1b452f5c6c6e84a118e60fd6f1168fd54849dc5c
unknown <viktormwandila@gmail.com>
2019-06-16 14:53:33 +0200

    initial commit

---
+++ js/insert.php
@@ -0,0 +1,27 @@
+<?php
+
+$db_conx = mysqli_connect("localhost", "root", "", "restaurants");
+// Evaluate the connection
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+    exit();
+}
+
+//store product
+if($_POST['action'] == 'store') {
+    //get product views
+    $orderItems = $_POST['orderItems'];
+    //insert views in database table
+    
+    $result = mysqli_query($db_conx, "INSERT INTO orders(orderItems) VALUES('$orderItems')")
+    or die('Order could not be placed.');
+
+    if($result) {
+        //get ID of insert product and echo it as json
+        $id = mysqli_insert_id();
+        header('Content-Type: application/json');
+        echo json_encode($id);
+    }
+}
+
+?>
